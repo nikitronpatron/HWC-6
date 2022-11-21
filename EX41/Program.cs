dc0d8f246c565c98c7163679327b3e6c2a0921aa
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-21 16:03:29 +0300

    EX41

---
+++ EX41/Program.cs
@@ -0,0 +1,27 @@
+﻿ // Пользователь вводит с клавиатуры M чисел. 
+ // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+bool check = false;
+int count = 0;
+int number = 0;
+string inputEnd = string.Empty;
+Console.WriteLine("Enter numbers separated by an 'Enter'");
+Console.WriteLine("For the end of input, enter 'end'");
+while(check == false)
+{
+    inputEnd = Console.ReadLine();
+    if(inputEnd == "end")
+    {
+        check = true;
+    }
+    else
+    {
+        number = Convert.ToInt32(inputEnd);
+        if(number > 0)
+        {
+            count++;
+            //Console.WriteLine($"Count = {count}");
+        }
+    }
+}
+Console.WriteLine($"Count = {count}");
